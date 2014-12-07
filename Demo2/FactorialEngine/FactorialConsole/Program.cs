588ba04779c4f2844fe1fb87e9a7c5ef4fd79801
Camilo Sanchez <camilin87@gmail.com>
2014-12-06 22:24:13 -0500

    use the enhanced factorial calculator

---
+++ Demo2/FactorialEngine/FactorialConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactorialEngine;
+
+namespace FactorialConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factorialResult = new FactorialCalculator(new FactorialWebService())
+                .Calculate(1000);
+            
+            Console.WriteLine("The factorial is {0}", factorialResult);
+            Console.ReadLine();
+        }
+    }
+}
