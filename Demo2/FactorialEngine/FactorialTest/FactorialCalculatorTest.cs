25e577f4ad455adcca5ead06c634516daadd9b81
Camilo Sanchez <camilin87@gmail.com>
2014-12-06 21:47:28 -0500

    create first ugly test

---
+++ Demo2/FactorialEngine/FactorialTest/FactorialCalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FactorialEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactorialTest
+{
+    [TestClass]
+    public class FactorialCalculatorTest
+    {
+        [TestMethod]
+        public void Calculates20KFactorial()
+        {
+            var actualValue = new FactorialCalculator().Calculate(20000);
+            Assert.AreEqual("really big number", actualValue);
+        }
+    }
+}
