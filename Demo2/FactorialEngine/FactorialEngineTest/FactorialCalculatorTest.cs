7be6feca0107688f6025c371b873438fa23177c7
Camilo Sanchez <camilin87@gmail.com>
2014-12-08 19:56:55 -0500

    remove duplication in tests

--- Demo2/FactorialEngine/FactorialEngineTest/FactorialCalculatorTest.cs
+++ Demo2/FactorialEngine/FactorialEngineTest/FactorialCalculatorTest.cs
@@ -8,24 +8,15 @@ namespace FactorialEngineTest
     public class FactorialCalculatorTest
     {
         [TestMethod]
-        public void CallsTheFactorialWebService()
+        public void CallsAWebServiceToCalculateTheFactorial()
         {
             var webService = MockRepository.GenerateMock<IWebServiceWrapper>();
-
-            new FactorialCalculator(webService).Calculate(1000);
-
-            webService.AssertWasCalled(w => w.ReadUrl("https://murmuring-ravine-8545.herokuapp.com/?n=1000"));
-        }
-
-        [TestMethod]
-        public void ReadsTheWebServiceResult()
-        {
-            var webService = MockRepository.GenerateMock<IWebServiceWrapper>();
-            webService.Stub(w => w.ReadUrl(Arg<string>.Is.Anything)).Return("my factorial");
+            webService.Stub(w => w.ReadUrl("https://murmuring-ravine-8545.herokuapp.com/?n=1000"))
+                .Return("my- factorial");
 
             var actualResult = new FactorialCalculator(webService).Calculate(1000);
 
-            Assert.AreEqual("my factorial", actualResult);
+            Assert.AreEqual("my- factorial", actualResult);
         }
     }
 }
