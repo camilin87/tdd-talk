40ed3b201cdd10c40e8a14288e7e68724645d560
Camilo Sanchez <camilin87@gmail.com>
2014-12-08 20:01:37 -0500

    use the factorial

---
+++ Demo2/FactorialEngine/FactorialConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactorialEngine;
+
+namespace FactorialConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factorial = new FactorialCalculator(new WebServiceWrapper()).Calculate(1000);
+
+            Console.WriteLine("The factorial is {0}", factorial);
+            Console.ReadLine();
+        }
+    }
+}
